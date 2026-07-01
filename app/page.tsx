cc3586984f8abd21f5889682034e56b3c50ed3fb
Dadir-Dev <196910954+Dadir-Dev@users.noreply.github.com>
2026-07-01 11:48:32 +0300

    feat(globals.css, page.tsx): update utility classes for gradients and dimensions in styles

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,14 @@
-import React from "react";
-
 const page = () => {
-  return <div>page</div>;
+  return (
+    <section>
+      <h1 className="text-center">
+        The Hub for Every Dev <br /> Event You Can&apos;t Miss
+      </h1>
+      <p className="text-center mt-5">
+        Hackathon, Workshop, Meetup, Conference, All in One Place
+      </p>
+    </section>
+  );
 };
 
 export default page;
