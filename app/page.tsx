4ecb0ea56dfdcf177baeeb160e97c8fc92bd425f
Dadir-Dev <196910954+Dadir-Dev@users.noreply.github.com>
2026-07-02 09:31:52 +0300

    feat(page.tsx): integrate EventCard component and display featured events

--- app/page.tsx
+++ app/page.tsx
@@ -1,4 +1,20 @@
 import ExploreBtn from "./components/ExploreBtn";
+import EventCard from "./components/EventCard";
+
+const events = [
+  {
+    image: "/images/event1.png",
+    title: "Event 1",
+  },
+  {
+    image: "/images/event2.png",
+    title: "Event 2",
+  },
+  {
+    image: "/images/event3.png",
+    title: "Event 3",
+  },
+];
 
 const page = () => {
   return (
@@ -15,9 +31,11 @@ const page = () => {
       <div className="mt-20 space-y-7">
         <h3>Featured Events</h3>
 
-        <ul>
-          {[1, 2, 3, 4, 5].map((event) => (
-            <li key={event}>Event {event}</li>
+        <ul className="events list-none">
+          {events.map((event) => (
+            <li key={event.title}>
+              <EventCard {...event} />
+            </li>
           ))}
         </ul>
       </div>
