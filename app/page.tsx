21695a81742d660619d4ffa4730114fb0ad2bd03
Dadir-Dev <196910954+Dadir-Dev@users.noreply.github.com>
2026-07-02 13:51:16 +0300

    refactor(page.tsx): replace local events array with imported events from constant

--- app/page.tsx
+++ app/page.tsx
@@ -1,20 +1,6 @@
 import ExploreBtn from "./components/ExploreBtn";
 import EventCard from "./components/EventCard";
-
-const events = [
-  {
-    image: "/images/event1.png",
-    title: "Event 1",
-  },
-  {
-    image: "/images/event2.png",
-    title: "Event 2",
-  },
-  {
-    image: "/images/event3.png",
-    title: "Event 3",
-  },
-];
+import { events } from "@/lib/constant";
 
 const page = () => {
   return (
