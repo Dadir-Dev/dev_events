07713d59f26c6337f3f9a001e4604a3b95426740
Dadir-Dev <196910954+Dadir-Dev@users.noreply.github.com>
2026-07-02 09:09:14 +0300

    feat(NavBar): add NavBar component and integrate it into the layout

---
+++ app/components/NavBar.tsx
@@ -0,0 +1,37 @@
+import Image from "next/image";
+import Link from "next/link";
+
+const NavBar = () => {
+  return (
+    <header>
+      <nav className="flex flex-row justify-between mx-auto container sm:px-10 px-5 py-4">
+        <Link href="/" className="flex flex-row items-center gap-2">
+          <Image
+            src="/icons/logo.png"
+            alt="logo"
+            width={32}
+            height={32}
+            className="logo"
+          />
+          <h1 className="text-xl font-bold italic max-sm:hidden px-0.5">
+            DevEvent
+          </h1>
+        </Link>
+
+        <ul className="flex md:gap-5 lg:gap-10 list-none">
+          <li>
+            <Link href="/events">Home</Link>
+          </li>
+          <li>
+            <Link href="/events">Events</Link>
+          </li>
+          <li>
+            <Link href="/about">Create Events</Link>
+          </li>
+        </ul>
+      </nav>
+    </header>
+  );
+};
+
+export default NavBar;
--- app/layout.tsx
+++ app/layout.tsx
@@ -3,6 +3,7 @@ import { Schibsted_Grotesk, Martian_Mono, Geist } from "next/font/google";
 import "./globals.css";
 import { cn } from "@/lib/utils";
 import LightRays from "./components/LightRays";
+import NavBar from "./components/NavBar";
 
 const geist = Geist({ subsets: ["latin"], variable: "--font-sans" });
 
@@ -39,6 +40,7 @@ export default function RootLayout({
       )}
     >
       <body className="min-h-screen flex flex-col bg-background text-foreground">
+        <NavBar />
         <div className="absolute inset-0 top-0 z-[-1] min-h-screen ">
           <LightRays
             raysOrigin="top-center-offset"
@@ -56,6 +58,7 @@ export default function RootLayout({
             saturation={1}
           />
         </div>
+
         <main>{children}</main>
       </body>
     </html>
